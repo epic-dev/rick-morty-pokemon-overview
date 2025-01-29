2cb9a4ecbff41fdc448bafb1b2c6fe91250c3aae
--get <pavel.yuruts@gmail.com>
2025-01-29 14:20:26 +0100

    removed unnecessary comments

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <UContainer class="py-9 h-full"> -->
   <div class="py-10">
     <UContainer class="m-auto mb-14 w-full">
       <div class="py-2 text-center">
@@ -25,15 +24,6 @@
         </h1>
       </div>
     </UContainer>
-    <!-- <div class="grid gap-4 xl:grid-cols-2">
-      <PageSection title="Rick and Morty API response">
-        <ExampleApiResponseRickAndMorty />
-      </PageSection>
-      <PageSection title="Pokemon API response">
-        <ExampleApiResponsePokemon />
-      </PageSection>
-    </div> -->
     <OverviewContent />
-  <!-- </UContainer> -->
   </div>
 </template>
